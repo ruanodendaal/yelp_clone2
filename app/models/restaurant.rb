7542c765aa25ea131ac84e8591bd2bf5b1b367bc
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-05-05 11:35:01 +0100

    create with user association extension module

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,11 +1,7 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews do
-    def build_with_user(attributes = {}, user)
-      attributes[:user] ||= user
-      build(attributes)
-    end
-  end
-
+  has_many :reviews,
+    -> { extending WithUserAssociationExtension },
+    dependent: :destroy
 
   belongs_to :user, dependent: :destroy
 
