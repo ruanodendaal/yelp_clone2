ab3f321da18d70cf73ee77f059dbb3708334f04c
Ruan Odendaal <ruanodendaal@gmail.com>
2017-05-05 10:32:20 +0100

    build review with user

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,10 @@ class Restaurant < ApplicationRecord
   belongs_to :user, dependent: :destroy
 
   validates :name, length: { minimum: 3 }, uniqueness: true
+
+  def build_review(review, user)
+    review = reviews.build(review)
+    review.user = user
+    review
+  end
 end
